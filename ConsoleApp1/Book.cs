5fa47728c3743718f75dfb57b2d18d869d1a139e
pablo <pablo@casi.dk>
2023-12-11 19:34:02 +0100

    adding Book class

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryProjectChallenge
+{
+    public class Book : LibraryItem
+    {
+        public string Author { get; private set; }
+        public int Pages { get; private set; }
+
+        public Book(string isbn, string title, string author, int pages) : base(isbn, title)
+        {
+            Author = author;
+            Pages = pages;
+        }
+
+        public override void DownloadFile()
+        {
+            Console.WriteLine("Downloading e-book file...");
+        }
+    }
+}
