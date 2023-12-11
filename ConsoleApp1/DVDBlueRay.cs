d9d6fe542101da63e3df5dee42db8e1d1979c653
pablo <pablo@casi.dk>
2023-12-11 19:38:19 +0100

    adding DVDBlueRay class

---
+++ ConsoleApp1/DVDBlueRay.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryProjectChallenge
+{
+    public class DVDBlueRay : Media
+    {
+        public DVDBlueRay(string isbn, string title) : base(isbn, title)
+        {
+        }
+
+        public override void DownloadFile()
+        {
+            Console.WriteLine("Downloading DVD/Blu-ray video files...");
+        }
+    }
+}
