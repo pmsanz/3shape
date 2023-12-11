837b1d9295e57e268cb8bc709abaec201f784a92
pablo <pablo@casi.dk>
2023-12-11 19:35:42 +0100

    Adding Track and Media class

---
+++ ConsoleApp1/Media.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryProjectChallenge
+{
+    public class Track
+    {
+        public string Title { get; set; } = String.Empty;
+        public string Artist { get; set; } = String.Empty;
+        public TimeSpan Duration { get; set; }
+    }
+
+    public abstract class Media : LibraryItem
+    {
+        public List<Track> Tracks { get; protected set; }
+        public Media(string isbn, string title) : base(isbn, title)
+        {
+            Tracks = new List<Track>();
+        }
+    }
+
+}
