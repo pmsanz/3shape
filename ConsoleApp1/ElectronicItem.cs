247d3bfe4aa574f109f2871c5d0d4d78046a194a
pablo <pablo@casi.dk>
2023-12-11 19:39:43 +0100

    adding ElectronicItem class

---
+++ ConsoleApp1/ElectronicItem.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryProjectChallenge
+{
+    public class ElectronicItem : LibraryItem
+    {
+        public ElectronicItem(string isbn, string title) : base(isbn, title)
+        {
+        }
+
+        public override void DownloadFile()
+        {
+            Console.WriteLine("Downloading electronic item file...");
+        }
+    }
+}
