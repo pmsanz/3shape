d12fa270a00cbeed848ad1befe3757117daeb419
pablo <pablo@casi.dk>
2023-12-11 20:14:00 +0100

    adding Test Project and modifiying book class author property

--- ConsoleApp1/Book.cs
+++ ConsoleApp1/Book.cs
@@ -2,14 +2,14 @@
 {
     public class Book : LibraryItem
     {
-        public List<string> Author { get; set; }
+        public List<string> Authors { get; set; }
         public string Publisher { get; private set; }
         public int Published { get; private set; }
         public int Pages { get; private set; }
         //Consider to create a class with a DTO instead a constructor with too many arguments.
         public Book(string isbn, string title, string author, string publisher, int pages, int published) : base(isbn, title)
         {
-            Author = new List<string> { author };
+            Authors = new List<string> { author };
             Pages = pages;
             Published = published;
             Publisher = publisher;
@@ -17,7 +17,7 @@
 
         public void AddAuthor(string author)
         {
-            Author.Add(author);
+            Authors.Add(author);
         }
         public override void DownloadFile()
         {
--- LibraryTestProject/UnitTest1.cs
+++ LibraryTestProject/UnitTest1.cs
@@ -1,11 +1,55 @@
+using LibraryProjectChallenge;
+
 namespace LibraryTestProject
 {
+
+    public static class Utils
+    {
+
+        public static string GetFilePath(string fileName)
+        {
+
+            string resourcePath = @"Resources\\";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            resourcePath = Path.Combine(currentDirectory, resourcePath);
+            string fullPath = Path.Combine(resourcePath, fileName);
+
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException();
+            }
+            return fullPath;
+
+        }
+
+    }
+
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ReadBooks_readFromFile_True()
         {
+            string fileName = "NewBooks.txt";
+            string fullFilePath = Utils.GetFilePath(fileName);
+            string input = File.ReadAllText(fullFilePath);
 
+            List<Book> result = BookManager.ReadBooks(input);
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+
+            Assert.Equal("Texts from Denmark", result[0].Title);
+            Assert.Equal("Brian Jensen", result[0].Authors[0]);
+            Assert.Equal("Gyldendal", result[0].Publisher);
+            Assert.Equal(2001, result[0].Published);
+            Assert.Equal(253, result[0].Pages);
+
+            Assert.Equal("Stories from abroad", result[1].Title);
+            Assert.Contains("Peter Jensen", result[1].Authors[0]);
+            Assert.Contains("Hans Andersen", result[1].Authors[1]);
+            Assert.Equal("Borgen", result[1].Publisher);
+            Assert.Equal(2012, result[1].Published);
+            Assert.Equal(156, result[1].Pages);
         }
     }
 }
