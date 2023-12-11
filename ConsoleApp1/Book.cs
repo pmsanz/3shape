d0c6603de0482d518c11ea1029d7033b265d1638
pablo <pablo@casi.dk>
2023-12-11 19:57:54 +0100

    Modifying Book class and creating BookManager to handle books.

--- ConsoleApp1/Book.cs
+++ ConsoleApp1/Book.cs
@@ -2,15 +2,23 @@
 {
     public class Book : LibraryItem
     {
-        public string Author { get; private set; }
+        public List<string> Author { get; set; }
+        public string Publisher { get; private set; }
+        public int Published { get; private set; }
         public int Pages { get; private set; }
-
-        public Book(string isbn, string title, string author, int pages) : base(isbn, title)
+        //Consider to create a class with a DTO instead a constructor with too many arguments.
+        public Book(string isbn, string title, string author, string publisher, int pages, int published) : base(isbn, title)
         {
-            Author = author;
+            Author = new List<string> { author };
             Pages = pages;
+            Published = published;
+            Publisher = publisher;
         }
 
+        public void AddAuthor(string author)
+        {
+            Author.Add(author);
+        }
         public override void DownloadFile()
         {
             Console.WriteLine("Downloading e-book file...");
---
+++ ConsoleApp1/BookManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LibraryProjectChallenge
+{
+    public static class BookManager
+    {
+        public static List<Book> ReadBooks(string input)
+        {
+            List<Book> books = new List<Book>();
+
+            // Define a regular expression pattern to match book sections
+            string pattern = @"Book:(.*?)\n\n";
+            var matches = Regex.Matches(input, pattern, RegexOptions.Singleline);
+
+            foreach (Match match in matches)
+            {
+                string bookInfo = match.Groups[1].Value;
+                Book book = ParseBook(bookInfo);
+                books.Add(book);
+            }
+
+            return books;
+        }
+
+        private static Book ParseBook(string bookInfo)
+        {
+            string[] lines = bookInfo.Split('\n').Select(line => line.Trim()).ToArray();
+            string author = "";
+            string title = "";
+            string publisher = "";
+            int pages = 0;
+            int published = 0;
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(':');
+                if (parts.Length != 2) continue;
+
+                string key = parts[0].Trim();
+                string value = parts[1].Trim();
+
+                switch (key)
+                {
+                    case "Author":
+                        author = value;
+                        break;
+                    case "Title":
+                        title = value;
+                        break;
+                    case "Publisher":
+                        publisher = value;
+                        break;
+                    case "NumberOfPages":
+                        if (int.TryParse(value, out int parsedPages))
+                        {
+                            pages = parsedPages;
+                        }
+                        break;
+                    case "Published":
+                        if (int.TryParse(value, out int parsedPublished))
+                        {
+                            published = parsedPublished;
+                        }
+                        break;
+                }
+            }
+
+            return new Book(Guid.NewGuid().ToString(), title, author, publisher, pages, published);
+
+        }
+    }
+}
