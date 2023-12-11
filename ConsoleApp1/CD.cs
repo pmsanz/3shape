8151bac12faaff0ddaf87f88d6d4575135ed31de
pablo <pablo@casi.dk>
2023-12-11 19:36:41 +0100

    adding CD class

---
+++ ConsoleApp1/CD.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryProjectChallenge
+{
+    public class CD : Media
+    {
+        public CD(string isbn, string title) : base(isbn, title)
+        {
+        }
+
+        public override void DownloadFile()
+        {
+            Console.WriteLine("Downloading CD audio files...");
+        }
+    }
+}
