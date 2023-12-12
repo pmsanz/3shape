4f1bcbedfa416b54db7ed2b8f0ac35f09ee8ade7
pablo <pablo@casi.dk>
2023-12-12 15:23:59 +0100

    modified book model; add LocationRegistration compounded class.

--- ConsoleApp1/Models/Book.cs
+++ ConsoleApp1/Models/Book.cs
@@ -6,6 +6,7 @@
         public string Publisher { get; private set; }
         public int Published { get; private set; }
         public int Pages { get; private set; }
+        public int ShelfNumber { get; private set; }
         //Consider to create a class with a DTO instead a constructor with too many arguments.
         public Book(List<string> authors, string isbn, string title, string publisher, int pages, int published) : base(isbn, title)
         {
@@ -13,11 +14,17 @@
             Pages = pages;
             Published = published;
             Publisher = publisher;
+
         }
 
         public void AddAuthor(string author)
         {
             Authors.Add(author);
+        }
+
+        public void SetShelf(int shelfNumber)
+        {
+            ShelfNumber = shelfNumber;
         }
         public override void DownloadFile()
         {
---
+++ ConsoleApp1/Models/LocationRegistration.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryProjectChallenge.Models
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public List<Row> Rows { get; set; }
+    }
+
+    public class Row
+    {
+        public int RowId { get; set; }
+        public int RoomId { get; set; }
+        public int RowNumber { get; set; }
+        public List<Bookshelf> Shelves { get; set; }
+    }
+
+    public class Bookshelf
+    {
+        public int RowId { get; set; }
+        public int ShelfNumber { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
